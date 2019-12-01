566e09dcb95bb98102406760c4181d99aa302aae
Eric Landeis <nightshadedude@gmail.com>
2019-12-01 01:50:33 -0700

    working on incorporating environment variables

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
-
 import (
-	t "D2GrailOBS/types"
+	"fmt"
+	"io/ioutil"
+	"log"
+	gc "solid-octo-lamp/googlesheetsconnector"
 )
 
-
-
-
-func main{
-	_ := 1
+func main() {
+	credentialsFileName := "cred.json"
+	sheetURL := ""
+	tabName := "TestSheet"
+	startColumn := 1
+	startRow := 1
+	endColumn := 4
+	endRow := 0
+	b, err := ioutil.ReadFile(credentialsFileName)
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+	fmt.Println(gc.GoogleSheetsConnector(sheetURL, tabName, startColumn, startRow, endColumn, endRow, b))
 }
